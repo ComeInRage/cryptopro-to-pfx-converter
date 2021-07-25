5df4146ef7faeefb4321055f9ccdde7ec5c11e9b
ComeInRage <montekek011@mail.ru>
2021-07-25 15:09:51 +0300

    Added script.sh

---
+++ script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+certs=$1
+pass=$2
+getcp=$3
+pems=$HOME/Desktop/pems/
+results=$HOME/Desktop/results/
+
+if ! [ -d $pems ]; then
+	mkdir $pems
+fi
+
+if ! [ -d $results ]; then
+	mkdir $results
+fi
+
+for file in $certs*.000
+do
+
+	if [ -d $file ]; then
+
+		pemfile=$pems${file##*/}.pem
+		resfile=$results${file##*/}.pfx
+
+		cd $getcp
+		./get-cpcert $certs $pass > $pemfile
+		
+		sudo openssl pkcs12 -engine gost -export -in $pemfile -out $resfile -keypbe gost89 -certpbe gost89 -macalg md_gost12_512
+
+	fi
+
+done
